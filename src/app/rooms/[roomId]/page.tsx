ff717b54947b08d5880e116a031810bc40632c0e
Shashank Kumar Gupta <91917475+shashank-2310@users.noreply.github.com>
2024-03-30 14:36:50 +0530

    adding in the video component from stream

--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -1,46 +1,45 @@
+import { splitTags, TagsList } from "@/components/tags-list";
 import { getRoom } from "@/services/rooms";
 import { Github } from "lucide-react";
 import Link from "next/link";
-import { Badge } from "@/components/ui/badge"
-import { splitTags, TagsList } from "@/components/tags-list";
-
+import { CoPilotVideo } from "./video-player";
+import { unstable_noStore } from "next/cache";
 
 export default async function RoomPage(props: { params: { roomId: string } }) {
+    unstable_noStore();
     const roomId = props.params.roomId;
     const room = await getRoom(roomId);
 
     if (!room) {
-        return <div className="">
-            No room of this ID found. Please check the URL or try again later.
-        </div>
+        return <div>No room of this ID found</div>;
     }
 
     return (
         <div className="grid grid-cols-4 min-h-screen">
             <div className="col-span-3 p-4 pr-2">
-                <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-4">
-                    video player
+                <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-4 min-h-screen">
+                    <CoPilotVideo room={room} />
                 </div>
             </div>
+
             <div className="col-span-1 p-4 pl-2">
                 <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-4 flex flex-col gap-4">
-                    <h1 className="text-base">
-                        {room.name}
-                    </h1>
-                    {room.githubRepo &&
+                    <h1 className="text-base">{room?.name}</h1>
+
+                    {room.githubRepo && (
                         <Link
-                            className="flex items-center gap-2 text-sm"
                             href={room.githubRepo}
+                            className="flex items-center gap-2 text-center text-sm"
                             target="_blank"
                             rel="noopener noreferrer"
                         >
-                            <Github /> Github Project
+                            <Github />
+                            Github Project
                         </Link>
-                    }
-                    <p className="text-base text-gray-600">
-                        {room.description}
-                    </p>
-                    <h3>Tags:</h3>
+                    )}
+
+                    <p className="text-base text-gray-600">{room?.description}</p>
+
                     <TagsList tags={splitTags(room.tag)} />
                 </div>
             </div>
